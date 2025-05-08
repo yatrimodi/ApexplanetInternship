651d7897b14f7c5e7e3c3e50b633ad9df70b75b7
yatrimodi <yatri1610@gmail.com>
2025-05-08 21:41:47 +0530

    new

---
+++ Task-1/script.js
@@ -0,0 +1,12 @@
+
+       const messages = [
+           "Believe in yourself! 🌈",
+           "Small steps lead to big results! 🏆",
+           "You’re stronger than you think! 💪"
+       ];
+
+       function showMessage() {
+           const randomIndex = Math.floor(Math.random() * messages.length);
+           alert(messages[randomIndex]);
+       }
+   
